6278ba7c320ce7a84112ae63d6e740e0b40f3f6f
MackLatimer <mack@cerberuscampaigns.com>
2025-08-12 10:17:25 -0500

    Fix: Clean up .gitignore and untrack ignored files

--- cerberus_universal_backend/app/models/shared_mixins.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.sql import func
-from ..extensions import db
-
-class TimestampMixin:
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
-    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
